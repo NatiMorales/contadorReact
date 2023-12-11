bd53479e4252a8ea455872d08f46e9925cbf4162
Natalia Morales <natii.vmorales98@gmail.com>
2023-12-11 10:36:02 -0300

    agregamos el componente TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import TituloSecundario from "./componentes/TituloSecundario";
 
 function App() {
+  //aqui agreagamos toda la logica que necesite el componente
   return (
-    <h1 className="display-3">Titulo de prueba</h1>
+    //aqui puedo agregar un poquito mas de logica
+    <>
+      {/* aqui empiezo a maquetar */}
+      <section className="container my-4">
+        <h1 className="display-3 text-center">Contador con React</h1>
+        <TituloSecundario></TituloSecundario>
+        <TituloSecundario/>
+      </section>
+    </>
   )
 }
 
-export default App
+export default App;
